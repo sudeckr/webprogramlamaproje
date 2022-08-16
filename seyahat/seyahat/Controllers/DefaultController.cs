c3ad0170f4606faf8fb809e56161678524fa9693
sudeckr <sudecakir69@hotmail.com>
2022-08-16 23:46:41 +0300

    change

---
+++ seyahat/seyahat/Controllers/DefaultController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace seyahat.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
